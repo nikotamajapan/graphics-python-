c7fb5a04159dcd0418d16f408dd97debe640ef88
mb1 <nikotamajapan@gmail.com>
2023-02-05 06:05:23 +0900

    graphics python turtle

---
+++ main.py
@@ -0,0 +1,40 @@
+import turtle
+
+# 1 pyramid
+#
+# for i in range(200):
+#     turtle.fd(i)
+#     turtle.left(360/4+30)
+
+# # 2 flower
+# for i in range(200):
+#     turtle.fd(i)
+#     turtle.left(360/4+10)
+
+# 3 green red
+# turtle.pencolor('green')
+# for i in range(60):
+#     turtle.fd(50)
+#     turtle.left(360/3+10)
+# turtle.pencolor('red')
+# for i in range(60):
+#     turtle.fd(200)
+#     turtle.left(360/3+10)
+
+# turtle.color('red','yellow')
+# turtle.begin_fill()
+#
+# for i in range(5*3):
+#     turtle.forward(100+i*20)
+#     turtle.right(360/5*2)
+# turtle.end_fill()
+
+# turtle.color('red','yellow')
+# turtle.begin_fill()
+#
+# for i in range(4):
+#     turtle.forward(100)
+#     turtle.right(90)
+# turtle.end_fill()
+
+turtle.done()
